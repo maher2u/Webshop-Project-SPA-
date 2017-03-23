a78ec39dc06902879637fff5e93151ac03b4b803
PFinnfors <pier@live.se>
2017-03-23 10:16:30 +0100

    Add project files.

---
+++ WebshopProject/Controllers/StartController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebshopProject.Models;
+
+namespace WebshopProject.Controllers
+{
+    public class StartController : Controller
+    {
+        // GET: Start
+        public ActionResult Index()
+        {
+            ModelClass shopModel = new ModelClass();
+
+            return View(shopModel);
+        }
+    }
+}
