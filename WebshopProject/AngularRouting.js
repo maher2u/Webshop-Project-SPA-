44a76400cd0f3a97dd73df19555e9e5a2f72479d
PFinnfors <pier@live.se>
2017-03-27 15:18:00 +0200

    Designing front-end

--- WebshopProject/AngularRouting.js
+++ WebshopProject/AngularRouting.js
@@ -1,22 +1,16 @@
-﻿(function () {
-
-    'use strict';
-
-    var app = angular.module("myApp", ["ngRoute"]);
-    app.config(function ($routeProvider) {
-        $routeProvider.
-        when("#/", {
-            templateUrl: "Products/Start"
-        }).
-        when("#/1", {
-            templateUrl: "Products/Product1"
-        }).
-        when("#/2", {
-            templateUrl: "Products/Product2"
-        }).
-        when("#/3", {
-            templateUrl: "Products/Product3"
-        });
+﻿var app = angular.module("shopApp", ["ngRoute"]);
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "/Products/Start"
+    })
+    .when("/1", {
+        templateUrl: "/Products/Product1"
+    })
+    .when("/2", {
+        templateUrl: "/Products/Product2"
+    })
+    .when("/3", {
+        templateUrl: "/Products/Product3"
     });
-
-})();
+});
