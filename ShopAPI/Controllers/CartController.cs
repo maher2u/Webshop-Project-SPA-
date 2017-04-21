281eb062fc121788dc9b2842d4e0670edb0326da
PFinnfors <pier@live.se>
2017-04-21 13:41:37 +0200

    Created a WebAPI

---
+++ ShopAPI/Controllers/CartController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebshopProject.Models;
+
+namespace ShopAPI.Controllers
+{
+    public class CartController : ApiController
+    {
+        //Webshop database model
+        private ModelClass db = new ModelClass();
+
+        // GET api/Cart
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/Cart/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/Cart
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/Cart/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/Cart/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- WebshopProject/AngularRouting.js
+++ WebshopProject/AngularRouting.js
@@ -1,16 +1,23 @@
-﻿var app = angular.module("shopApp", ["ngRoute"]);
-app.config(function ($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl: "/Products/Start"
-    })
-    .when("/JL", {
-        templateUrl: "/Products/JL"
-    })
-    .when("/2", {
-        templateUrl: "/Products/Product2"
-    })
-    .when("/top-2017", {
-        templateUrl: "/Products/Top2017"
-    });
-});
+﻿(function () {
+    'use strict';
+
+    angular.module("shopApp", ["ngRoute"]).
+        config(function ($routeProvider) {
+            $routeProvider
+                .when("/", {
+                    templateUrl: "/Products/Start"
+                })
+                .when("/JL", {
+                    templateUrl: "/Products/JL"
+                })
+                .when("/2", {
+                    templateUrl: "/Products/Product2"
+                })
+                .when("/top-2017", {
+                    templateUrl: "/Products/Top2017"
+                });
+        });
+
+
+    
+})();
